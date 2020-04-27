60dcb40944de819b4740f366003fc8a80e6f6d70
lnascimento01 <lfsnascimento84@gmail.com>
2020-04-26 22:13:42 -0300

    Documentação da controller

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -9,6 +9,10 @@ class AuthenticationController extends Controller {
 
     public $loginAfterSignUp = true;
 
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function register(Request $request) {
         $user = User::create([
             'name' => $request->name,
@@ -21,6 +25,10 @@ class AuthenticationController extends Controller {
         return $this->respondWithToken($token);
     }
 
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function login(Request $request) {
         $credentials = $request->only(['email', 'password']);
 
@@ -31,21 +39,30 @@ class AuthenticationController extends Controller {
         return $this->respondWithToken($token);
     }
 
-    public function getAuthUser(Request $request) {
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getAuthUser() {
         return response()->json(auth()->user());
     }
 
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function logout() {
         auth()->logout();
         return response()->json(['message' => 'Successfully logged out']);
     }
 
+    /**
+     * @param $token
+     * @return \Illuminate\Http\JsonResponse
+     */
     protected function respondWithToken($token) {
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
+            'expires_in' => auth()->factory()->getTTL() * 360
         ]);
     }
-
 }
